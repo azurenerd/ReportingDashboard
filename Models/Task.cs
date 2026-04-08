1f2e57cfb2876a81318ecfcc0d30bb885cb08f98
azurenerd <azurenerd@users.noreply.github.com>
2026-04-08 02:59:58 -0700

    Address review feedback

--- Models/Task.cs
+++ Models/Task.cs
@@ -4,7 +4,7 @@ public class Task
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string AssignedOwner { get; set; } = string.Empty;
+    public string AssignedTo { get; set; } = string.Empty;
     public TaskStatus Status { get; set; }
 }
 
