9a01cac1195317c77e9da1b6dec9f252d15edbeb
azurenerd <azurenerd@users.noreply.github.com>
2026-04-08 02:40:32 -0700

    PrincipalEngineer: Implement ProjectDataService (#124)
    Merged by ProgramManager after dual approval from ProgramManager and Architect

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using AgentSquad.Runner.Components;
 using AgentSquad.Runner.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,30 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddLogging(logging =>
-{
-    if (builder.Environment.IsDevelopment())
-    {
-        logging.AddConsole();
-        logging.SetMinimumLevel(LogLevel.Debug);
-    }
-    else
-    {
-        logging.AddEventSourceLogger();
-        logging.SetMinimumLevel(LogLevel.Information);
-    }
-});
-
-builder.Services.AddSingleton<ProjectDataService>();
-
-builder.Services.AddCircuitOptions(options =>
-{
-    options.DisconnectedCircuitMaxRetained = 100;
-    if (!builder.Environment.IsDevelopment())
-    {
-        options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(3);
-    }
-});
+builder.Services.AddScoped<ProjectDataService>();
 
 var app = builder.Build();
 
@@ -40,13 +16,11 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
 
 app.Run();
