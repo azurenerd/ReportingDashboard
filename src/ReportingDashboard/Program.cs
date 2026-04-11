c3d41ab5b84179c41fb3580ba35fb78f14d39c63
azurenerd <azurenerd@users.noreply.github.com>
2026-04-11 11:30:52 -0500

    Step 1/6: Create project scaffold: `src/ReportingDashboard/` directory structure w...

--- src/ReportingDashboard/Program.cs
+++ src/ReportingDashboard/Program.cs
@@ -1,23 +1,16 @@
-using ReportingDashboard.Components;
-using ReportingDashboard.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://localhost:5000");
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddSingleton<DashboardDataService>();
 
 var app = builder.Build();
-
-var dataService = app.Services.GetRequiredService<DashboardDataService>();
-await dataService.LoadAsync(Path.Combine(app.Environment.WebRootPath, "data.json"));
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
+app.MapRazorComponents<ReportingDashboard.Components.App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
