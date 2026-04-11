76f258c2f40cc6c5d9be16ce67d801418a9d0805
AgentSquad <agentsquad@noreply.github.com>
2026-04-11 17:02:35 -0500

    Step 1/6: **Solution scaffolding and .gitignore:** Update `.gitignore` with .NET e...

--- ReportingDashboard/Program.cs
+++ ReportingDashboard/Program.cs
@@ -1,15 +1,12 @@
-using ReportingDashboard.Components;
-using ReportingDashboard.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents();
-builder.Services.AddSingleton<DashboardDataService>();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-app.MapRazorComponents<App>();
+
+app.MapRazorComponents<ReportingDashboard.Components.App>();
 
 app.Run();
