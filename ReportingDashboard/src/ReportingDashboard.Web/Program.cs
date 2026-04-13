46d215495bb9667b7c10a7f9c33f78c2ce540079
azurenerd <azurenerd@users.noreply.github.com>
2026-04-13 12:37:58 -0500

    PrincipalEngineer 2: Categorized Status Sections Component (#1056)
    Merged by PrincipalEngineer after approval from ProgramManager and PrincipalEngineer and Architect

--- ReportingDashboard/src/ReportingDashboard.Web/Program.cs
+++ ReportingDashboard/src/ReportingDashboard.Web/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<IDashboardDataService, DashboardDataService>();
+builder.Services.AddSingleton<DashboardDataService>();
 
 var app = builder.Build();
 
