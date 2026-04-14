f8abc01192360a9ee6bc95a94c8aad74ac1cabab
azurenerd <azurenerd@users.noreply.github.com>
2026-04-13 22:17:56 -0500

    Rebase: PrincipalEngineer: Build milestone timeline SVG component (#1149)
    Merged by PrincipalEngineer after branch sync and approval from ProgramManager and PrincipalEngineer and Architect

--- ReportingDashboard/Program.cs
+++ ReportingDashboard/Program.cs
@@ -5,16 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
 builder.Services.AddSingleton<DashboardDataService>();
-
-builder.WebHost.UseUrls("http://localhost:5000", "https://localhost:5001");
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
