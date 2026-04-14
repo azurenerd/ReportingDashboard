d4056f3c8177f3f03f8d4e1ad6da6e2701c551a0
azurenerd <azurenerd@users.noreply.github.com>
2026-04-14 15:29:35 -0500

    Reset repo for fresh agent run
    Preserved: .gitignore, OriginalDesignConcept.html

--- src/ReportingDashboard/Services/DashboardDataException.cs
+++
@@ -1,9 +0,0 @@
-namespace ReportingDashboard.Services;
-
-public class DashboardDataException : Exception
-{
-    public DashboardDataException(string message) : base(message) { }
-
-    public DashboardDataException(string message, Exception innerException)
-        : base(message, innerException) { }
-}
