6d6dc7ef7259831b0bd2e1cd33ba4d416ae4134c
AgentSquad Reset <agentsquad-cleanup@noreply>
2026-04-21 09:16:09 -0500

    Minimal reset — preserved 4 startup doc(s), removed 44 other file(s)

--- src/ReportingDashboard.Web/Models/HeatmapCategory.cs
+++
@@ -1,8 +0,0 @@
-namespace ReportingDashboard.Web.Models;
-
-public class HeatmapCategory
-{
-    public string Name { get; set; } = "";
-    public string ColorClass { get; set; } = "";
-    public Dictionary<string, List<string>> Items { get; set; } = new();
-}
