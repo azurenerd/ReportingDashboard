dff444ce185603215a4f70229698f27f5d75e7e6
azurenerd <azurenerd@users.noreply.github.com>
2026-04-21 01:11:27 -0500

    Rebase: SoftwareEngineer: Implement Implement page layout and CSS foundation for 1920x1080 screenshot

--- ReportingDashboard/Program.cs
+++ ReportingDashboard/Program.cs
@@ -1,5 +1,4 @@
 using ReportingDashboard.Components;
-using ReportingDashboard.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,19 +8,11 @@ builder.WebHost.UseUrls(
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton<DashboardDataService>(sp =>
-{
-    var config = sp.GetRequiredService<IConfiguration>();
-    var env = sp.GetRequiredService<IWebHostEnvironment>();
-    var dataPath = config.GetValue<string>("DataFilePath")
-        ?? Path.Combine(env.ContentRootPath, "Data", "data.json");
-    return new DashboardDataService(dataPath);
-});
-
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
