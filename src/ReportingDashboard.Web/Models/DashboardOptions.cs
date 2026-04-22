80f6cbb110e7b386ebd2dbb75d0f9343f154b029
AgentSquad Reset <agentsquad-cleanup@noreply>
2026-04-22 00:53:58 -0500

    Minimal reset — preserved 4 startup doc(s), removed 49 other file(s)

--- src/ReportingDashboard.Web/Models/DashboardOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace ReportingDashboard.Web.Models;
-
-public class DashboardOptions
-{
-    public string DataFilePath { get; set; } = "./data.json";
-}
--- src/ReportingDashboard.Web/Models/HeatmapData.cs
+++
@@ -1,8 +0,0 @@
-namespace ReportingDashboard.Web.Models;
-
-public class HeatmapData
-{
-    public List<string> Months { get; set; } = new();
-    public string CurrentMonth { get; set; } = "";
-    public List<HeatmapCategory> Categories { get; set; } = new();
-}
