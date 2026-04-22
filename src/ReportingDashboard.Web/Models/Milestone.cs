b61c1369154ced6d0159358cd2e6d807dbf4a182
AgentSquad Reset <agentsquad-cleanup@noreply>
2026-04-21 22:21:36 -0500

    Minimal reset — preserved 4 startup doc(s), removed 50 other file(s)

--- src/ReportingDashboard.Web/Models/Milestone.cs
+++
@@ -1,8 +0,0 @@
-namespace ReportingDashboard.Web.Models;
-
-public class Milestone
-{
-    public string Date { get; set; } = "";
-    public string Type { get; set; } = "";
-    public string Label { get; set; } = "";
-}
