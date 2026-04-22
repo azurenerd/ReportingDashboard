5caf1ef2bef8511932af1cac78fc1ca068279e91
azurenerd <azurenerd@users.noreply.github.com>
2026-04-22 04:21:05 -0500

    Reset: clean slate for new run

--- src/ReportingDashboard.Web/Models/TimelineTrack.cs
+++
@@ -1,9 +0,0 @@
-namespace ReportingDashboard.Web.Models;
-
-public class TimelineTrack
-{
-    public string Id { get; set; } = "";
-    public string Label { get; set; } = "";
-    public string Color { get; set; } = "";
-    public List<Milestone> Milestones { get; set; } = new();
-}
--- src/ReportingDashboard.Web/Services/IDataService.cs
+++
@@ -1,9 +0,0 @@
-using ReportingDashboard.Web.Models;
-
-namespace ReportingDashboard.Web.Services;
-
-public interface IDataService
-{
-    DashboardData? GetData();
-    string? GetError();
-}
